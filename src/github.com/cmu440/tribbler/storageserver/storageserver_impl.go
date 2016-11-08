358d45799f37264a3c618aa0ccbd01d06cdceeff
Max Ma <hflsmax@gmail.com>
2016-11-08 15:51:28 -0500

    Not good design

--- src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
+++ src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
@@ -8,16 +8,47 @@ import (
 	"strconv"
 	"sync"
 	"time"
-	// "fmt"
+	"strings"
+	"hash/fnv"
+	"fmt"
 )
 
+type callbackWithID struct {
+	ID int
+	hostPort string
+}
+
+type callbacks struct{
+	sync.Mutex
+	cbID int
+	list []callbackWithID
+}
+
+type value struct {
+	// sync.RWMutex
+	value interface{}
+	callbacks *callbacks
+	// live bool
+}
+
 type storageServer struct {
-	storage           map[string]interface{}
+	storage           map[string]value
 	storageMutex      *sync.Mutex
 	servers           []storagerpc.Node
 	serversMutex      *sync.Mutex
 	numNodes          int
 	allRegisterNotify chan bool
+	nodeID uint32
+}
+
+// StoreHash hashes a string key and returns a 32-bit integer. This function
+// is provided here so that all implementations use the same hashing mechanism
+// (both the Libstore and StorageServer should use this function to hash keys).
+func StoreHash(key string) uint32 {
+	prefix := strings.Split(key, ":")[0]
+	hasher := fnv.New32()
+	hasher.Write([]byte(prefix))
+	return hasher.Sum32()
 }
 
 // NewStorageServer creates and starts a new StorageServer. masterServerHostPort
@@ -30,9 +61,10 @@ type storageServer struct {
 // and should return a non-nil error if the storage server could not be started.
 func NewStorageServer(masterServerHostPort string, numNodes, port int, nodeID uint32) (StorageServer, error) {
 	ss := new(storageServer)
-	ss.storage = make(map[string]interface{})
+	ss.storage = make(map[string]value)
 	ss.storageMutex = &sync.Mutex{}
 	ss.serversMutex = &sync.Mutex{}
+	ss.nodeID = nodeID
 
 	if masterServerHostPort != "" {
 		// This is a slave
@@ -53,6 +85,18 @@ func NewStorageServer(masterServerHostPort string, numNodes, port int, nodeID ui
 			}
 			time.Sleep(time.Second)
 		}
+
+		listener, err := net.Listen("tcp", "127.0.0.1:"+strconv.Itoa(port))
+		if err != nil {
+			return nil, err
+		}
+		err = rpc.RegisterName("StorageServer", storagerpc.Wrap(ss))
+		if err != nil {
+			return nil, err
+		}
+		rpc.HandleHTTP()
+		go http.Serve(listener, nil)
+
 	} else {
 		// This is a master
 		ss.servers = []storagerpc.Node{storagerpc.Node{
@@ -73,8 +117,24 @@ func NewStorageServer(masterServerHostPort string, numNodes, port int, nodeID ui
 		go http.Serve(listener, nil)
 
 		<-ss.allRegisterNotify
+	}
+	return ss, nil
+}
 
-		return ss, nil
+func isCorrectNode(ss *storageServer, key string) bool {
+	hash := StoreHash(key)
+	var min uint32 = 4294967295
+	var node storagerpc.Node = ss.servers[0]
+	for _, item := range ss.servers {
+		if item.NodeID-hash < min {
+			node = item
+			min = item.NodeID - hash
+		}
+	}
+	if ss.nodeID == node.NodeID {
+		return true
+	} else {
+		return false
 	}
 }
 
@@ -117,24 +177,104 @@ func (ss *storageServer) GetServers(args *storagerpc.GetServersArgs, reply *stor
 	return nil
 }
 
+func installLease(ss *storageServer, key string, hostPort string) {
+	ss.storage[key].callbacks.Lock()
+	ss.storage[key].callbacks.cbID += 1
+	ss.storage[key].callbacks.list = append(ss.storage[key].callbacks.list, callbackWithID{ID: ss.storage[key].callbacks.cbID, hostPort: hostPort})
+	fmt.Println("install lease: ", key, hostPort)
+	ss.storage[key].callbacks.Unlock()
+
+
+	// Remove callback after a predetermined time
+	go func(hostPort string, idToRemove int) {
+		time.Sleep(time.Second*(storagerpc.LeaseSeconds+storagerpc.LeaseGuardSeconds))
+		fmt.Println("remove callback", hostPort)
+		ss.storage[key].callbacks.Lock()
+		toRemove := -1
+		for i, h := range ss.storage[key].callbacks.list {
+			if h.ID ==  idToRemove{
+				toRemove = i
+				break
+			}
+		}
+		if toRemove != -1 {
+			ss.storage[key].callbacks.list =
+				append(ss.storage[key].callbacks.list[:toRemove], ss.storage[key].callbacks.list[toRemove+1:]...)
+		}
+		ss.storage[key].callbacks.Unlock()
+	}(hostPort, ss.storage[key].callbacks.cbID)
+}
+
 func (ss *storageServer) Get(args *storagerpc.GetArgs, reply *storagerpc.GetReply) error {
+	if !isCorrectNode(ss, args.Key) {
+		reply.Status = storagerpc.WrongServer
+		return nil
+	}
+
 	ss.storageMutex.Lock()
 	if val, ok := ss.storage[args.Key]; ok {
+		// if !val.live {
+		// 	// value has been deleted
+		// 	reply.Status = storagerpc.KeyNotFound
+		// } else {
+		if args.WantLease {
+			installLease(ss, args.Key, args.HostPort)
+			reply.Lease = storagerpc.Lease{
+				Granted: true,
+				ValidSeconds: storagerpc.LeaseSeconds}
+		}
 		reply.Status = storagerpc.OK
-		reply.Value = val.(string)
+		fmt.Println("get", args.Key, ":", val.value.(string))
+		reply.Value = val.value.(string)
+		// }
 	} else {
+		fmt.Println("get", args.Key, ":", "keynotfound")
 		reply.Status = storagerpc.KeyNotFound
 	}
 	ss.storageMutex.Unlock()
 	return nil
 }
 
+func revokeLeases(ss *storageServer, key string) error {
+	ss.storage[key].callbacks.Lock()
+	for _, cb := range ss.storage[key].callbacks.list {
+		cli, err := rpc.DialHTTP("tcp", cb.hostPort)
+		if err != nil {
+			ss.storage[key].callbacks.Unlock()
+			return err
+		}
+		args := &storagerpc.RevokeLeaseArgs{}
+		args.Key = key
+		var reply storagerpc.RevokeLeaseReply
+		err = cli.Call("LeaseCallbacks.RevokeLease", args, &reply)
+		if err != nil {
+			ss.storage[key].callbacks.Unlock()
+			return err
+		}
+	}
+	ss.storage[key].callbacks.list = make([]callbackWithID,0)
+	ss.storage[key].callbacks.Unlock()
+	return nil
+}
+
 func (ss *storageServer) Delete(args *storagerpc.DeleteArgs, reply *storagerpc.DeleteReply) error {
+	if !isCorrectNode(ss, args.Key) {
+		reply.Status = storagerpc.WrongServer
+		return nil
+	}
+
 	ss.storageMutex.Lock()
 	if _, ok := ss.storage[args.Key]; ok {
+		if err := revokeLeases(ss, args.Key); err != nil {
+			ss.storageMutex.Unlock()
+			return err
+		}
+		// ss.storage[args.Key].live = false
 		delete(ss.storage, args.Key)
+		fmt.Println("delete ", args.Key)
 		reply.Status = storagerpc.OK
 	} else {
+		fmt.Println("delete ", args.Key, " KeyNotFound")
 		reply.Status = storagerpc.KeyNotFound
 	}
 	ss.storageMutex.Unlock()
@@ -142,36 +282,77 @@ func (ss *storageServer) Delete(args *storagerpc.DeleteArgs, reply *storagerpc.D
 }
 
 func (ss *storageServer) GetList(args *storagerpc.GetArgs, reply *storagerpc.GetListReply) error {
-	ss.storageMutex.Lock()
-	if val, ok := ss.storage[args.Key]; ok {
-		ss.storageMutex.Unlock()
-		reply.Status = storagerpc.OK
-		reply.Value = val.([]string)
-		return nil
-	} else {
-		reply.Status = storagerpc.KeyNotFound
+	if !isCorrectNode(ss, args.Key) {
+		reply.Status = storagerpc.WrongServer
 		return nil
 	}
+
+	ss.storageMutex.Lock()
+	if val, ok := ss.storage[args.Key]; ok {
+		// if !val.live {
+		// 	//value has been deleted
+		// 	reply.Status = storagerpc.KeyNotFound
+		// } else {
+		if args.WantLease {
+			installLease(ss, args.Key, args.HostPort)
+			reply.Lease = storagerpc.Lease{
+				Granted: true,
+				ValidSeconds: storagerpc.LeaseSeconds}
+		}
+		reply.Status = storagerpc.OK
+		fmt.Println("getlist ", args.Key, ": ", val.value.([]string))
+		reply.Value = val.value.([]string)
+		// }
+	} else {
+		fmt.Println("getlist ", args.Key, ": KeyNotFound")
+		reply.Status = storagerpc.KeyNotFound
+	}
+	ss.storageMutex.Unlock()
+	return nil
 }
 
 func (ss *storageServer) Put(args *storagerpc.PutArgs, reply *storagerpc.PutReply) error {
+	if !isCorrectNode(ss, args.Key) {
+		reply.Status = storagerpc.WrongServer
+		return nil
+	}
+
 	ss.storageMutex.Lock()
-	ss.storage[args.Key] = args.Value
+	if _, ok := ss.storage[args.Key]; ok {
+		if err := revokeLeases(ss, args.Key); err != nil {
+			ss.storageMutex.Unlock()
+			return err
+		}
+		fmt.Println("put update", args.Key, ":", args.Value)
+		ss.storage[args.Key] = value{value: args.Value, callbacks: &callbacks{cbID: 0, list: make([]callbackWithID,0)}}
+	} else {
+		fmt.Println("put", args.Key, ":", args.Value)
+		ss.storage[args.Key] = value{
+			callbacks: &callbacks{cbID: 0, list: make([]callbackWithID, 0)},
+			value: args.Value}
+	}
 	ss.storageMutex.Unlock()
 	reply.Status = storagerpc.OK
 	return nil
 }
 
 func (ss *storageServer) AppendToList(args *storagerpc.PutArgs, reply *storagerpc.PutReply) error {
+	if !isCorrectNode(ss, args.Key) {
+		reply.Status = storagerpc.WrongServer
+		return nil
+	}
+
 	ss.storageMutex.Lock()
 	if _, ok := ss.storage[args.Key]; !ok {
 		// if key not found, create it
-		ss.storage[args.Key] = []string{args.Value}
+		ss.storage[args.Key] = value{
+			callbacks: &callbacks{cbID: 0, list: make([]callbackWithID, 0)},
+			value: []string{args.Value}}
 		reply.Status = storagerpc.OK
 		ss.storageMutex.Unlock()
 		return nil
 	}
-	l := ss.storage[args.Key].([]string)
+	l := ss.storage[args.Key].value.([]string)
 	for _, val := range l {
 		if val == args.Value {
 			reply.Status = storagerpc.ItemExists
@@ -181,18 +362,27 @@ func (ss *storageServer) AppendToList(args *storagerpc.PutArgs, reply *storagerp
 	}
 	// if key found and value not exists
 	l = append(l, args.Value)
-	ss.storage[args.Key] = l
+	if err := revokeLeases(ss, args.Key); err != nil {
+		ss.storageMutex.Unlock()
+		return err
+	}
+	ss.storage[args.Key] = value{value: l, callbacks: &callbacks{cbID: 0, list: make([]callbackWithID,0)}}
 	reply.Status = storagerpc.OK
 	ss.storageMutex.Unlock()
 	return nil
 }
 
 func (ss *storageServer) RemoveFromList(args *storagerpc.PutArgs, reply *storagerpc.PutReply) error {
+	if !isCorrectNode(ss, args.Key) {
+		reply.Status = storagerpc.WrongServer
+		return nil
+	}
+
 	ss.storageMutex.Lock()
 	if l, ok := ss.storage[args.Key]; !ok {
 		reply.Status = storagerpc.KeyNotFound
 	} else {
-		l := l.([]string)
+		l := l.value.([]string)
 		iToDelete := -1
 		for i, val := range l {
 			if val == args.Value {
@@ -202,7 +392,11 @@ func (ss *storageServer) RemoveFromList(args *storagerpc.PutArgs, reply *storage
 		}
 		if iToDelete != -1 {
 			l = append(l[:iToDelete], l[iToDelete+1:]...)
-			ss.storage[args.Key] = l
+			if err := revokeLeases(ss, args.Key); err != nil {
+				ss.storageMutex.Unlock()
+				return err
+			}
+			ss.storage[args.Key] = value{value: l, callbacks: &callbacks{cbID: 0, list: make([]callbackWithID,0)}}
 			reply.Status = storagerpc.OK
 		} else {
 			reply.Status = storagerpc.ItemNotFound
